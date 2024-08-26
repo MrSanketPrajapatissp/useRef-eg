6dcab67f2341e61930d4a52560ff95695bd4883d
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2024-08-26 21:47:40 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 function App() {
   const [counter, setCount] = useState(0);
   const [inputvalue, setInputValue] = useState(1);
 
-  let count = 0;
-  for (let i = 1; i <= inputvalue; i++) {
-    count = count + i;
-  }
+  let count = useMemo(() => {
+    let finalCount = 0;
+    for (let i = 1; i <= inputvalue; i++) {
+      finalCount = finalCount + i;
+    }
+    return finalCount;
+  }, [inputvalue]);
 
   return (
     <div>
@@ -15,6 +18,7 @@ function App() {
         onChange={function (e) {
           setInputValue(e.target.value);
         }}
+        placeholder="{find sum from 1 to n}"
       ></input>
       <br />
       Sum from 1 to {inputvalue} is {count}
