6590b5f69761d4fbc8616ab06a1cfc1ec5ac5cea
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2024-08-26 21:04:02 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,33 @@
-import { useEffect } from "react";
 import { useState } from "react";
-import axios from "axios";
-import PropTypes from "prop-types";
 
 function App() {
-  const [selectedId, setid] = useState(1);
+  const [counter, setCount] = useState(0);
+  const [inputvalue, setInputValue] = useState(1);
+
+  let count = 0;
+  for (let i = 1; i <= inputvalue; i++) {
+    count = count + i;
+  }
 
   return (
     <div>
+      <input
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+      ></input>
+      <br />
+      Sum from 1 to {inputvalue} is {count}
+      <br />
       <button
         onClick={function () {
-          setid(1);
+          setCount(counter + 1);
         }}
       >
-        1
+        Counter {counter}
       </button>
-      <button
-        onClick={function () {
-          setid(2);
-        }}
-      >
-        2
-      </button>
-      <button
-        onClick={function () {
-          setid(3);
-        }}
-      >
-        3
-      </button>
-      <button
-        onClick={function () {
-          setid(4);
-        }}
-      >
-        4
-      </button>
-
-      <Todo id={selectedId} />
     </div>
   );
 }
 
-function Todo({ id }) {
-  const [getItem, setItem] = useState({});
-
-  useEffect(() => {
-    setTimeout(() => {
-      axios.get(`https://dummyjson.com/todos/` + id).then((response) => {
-        setItem(response.data);
-      });
-    }, 3000);
-  }, [id]);
-
-  return (
-    <div>
-      Id: {id}
-      <h3>{getItem.todo}</h3>
-      {/* <h3>{Item.id}</h3>
-      <h3>{Item.completed}</h3> */}
-    </div>
-  );
-}
 export default App;
-
-Todo.propTypes = {
-  id: PropTypes.number.isRequired,
-};
