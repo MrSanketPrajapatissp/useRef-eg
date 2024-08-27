34ca370c0159d3d46b916e7f91cb0739b9296cbf
MrSanketPrajapatissp <125129649+mrsanketprajapati@users.noreply.github.com>
2024-08-27 15:54:53 +0530

    first memo use

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
-import { useMemo, useState } from "react";
+import { memo, useState } from "react";
 
 function App() {
   const [counter, setCount] = useState(0);
-  const [inputvalue, setInputValue] = useState(1);
-
-  let count = useMemo(() => {
-    let finalCount = 0;
-    for (let i = 1; i <= inputvalue; i++) {
-      finalCount = finalCount + i;
-    }
-    return finalCount;
-  }, [inputvalue]);
 
   return (
     <div>
-      <input
-        onChange={function (e) {
-          setInputValue(e.target.value);
-        }}
-        placeholder="{find sum from 1 to n}"
-      ></input>
-      <br />
-      Sum from 1 to {inputvalue} is {count}
-      <br />
       <button
-        onClick={function () {
+        onClick={() => {
           setCount(counter + 1);
         }}
       >
         Counter {counter}
       </button>
+
+      <ButtonComponent />
     </div>
   );
 }
 
+const ButtonComponent = memo(() => {
+  console.log("Child Render");
+
+  return (
+    <div>
+      <button>button Clicked</button>
+    </div>
+  );
+});
+
+ButtonComponent.displayName = "ButtonComponent";
 export default App;
